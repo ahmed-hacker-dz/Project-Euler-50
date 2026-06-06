1befc3c1eea1121947e2303dacfdd1e1a171359b
ahmed-hacker-dz <ahmedguesmi202555@gmail.com>
2026-06-06 09:27:37 +0100

    Create    problem_1.py

---
+++ problem_1.py
@@ -0,0 +1,12 @@
+ def calculate_total_sum(limit):
+    def sum_of_multiples(x, max_val):
+       n = max_val // x
+       return x * n * (n + 1) // 2 
+                   
+    sum_3  = sum_of_multiples(3, limit) 
+    sum_5  = sum_of_multiples(5, limit)  
+    sum_15 = sum_of_multiples(15, limit) 
+
+    return sum_3 + sum_5 - sum_15
+   
+ print(calculate_total_sum(1000))
